cf22dbf3704bea6d195f07a72fc3281ced062a75
JoanRibot <jribot@cifpfbmoll.eu>
2021-02-24 14:32:49 +0100

    arreglando problema duplicacion loadCoins

--- pigCoin/pigCoin/src/main/java/pigCoin/Wallet.java
+++ pigCoin/pigCoin/src/main/java/pigCoin/Wallet.java
@@ -2,6 +2,8 @@ package pigCoin;
 
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Wallet {
     private PublicKey address;
@@ -9,6 +11,8 @@ public class Wallet {
     private double total_input = 0.0d;
     private double total_output = 0.0d;
     private double balance = 0.0d;
+    private List<Transaction> inputTransactions = new ArrayList<>();
+    private List<Transaction> outputTransactions = new ArrayList<>();
 
 	public void setSK(PrivateKey sKey) {
                 this.sKey = sKey;
@@ -45,6 +49,18 @@ public class Wallet {
                 this.total_input += input;
         }    
 
+        public void loadInputTransactions(BlockChain bChain) {
+                bChain.getBlockChain().stream().filter(t -> t.getpKey_recipent().equals(this.address)).filter(t -> this.inputTransactions.indexOf(t) == -1).forEach(t -> {
+                        this.inputTransactions.add(t);
+                });
+        }
+
+        public void loadOutputTransactions(BlockChain bChain){
+                bChain.getBlockChain().stream().filter(t -> t.getpKey_sender().equals(this.address)).filter(t -> this.outputTransactions.indexOf(t) == -1).forEach(t -> {
+                        this.outputTransactions.add(t);
+                });
+        }
+        
         private void setTotal_output(double output){
                 this.total_output += output;
         }
@@ -53,19 +69,26 @@ public class Wallet {
                 this.balance = this.total_input - this.total_output;
         }
 
+        public List<Transaction> getInputTransactions() {
+                return this.inputTransactions;
+        }
+
+        public List<Transaction> getOutputTransactions(){
+                return this.outputTransactions;
+        }
+
         public void loadCoins(BlockChain bChain) {
-                bChain.getBlockChain().stream().filter(t -> t.getpKey_recipent().equals(this.address)).forEach(t -> {
+                bChain.getBlockChain().stream().filter(t -> t.getpKey_recipent().equals(this.address)).filter(t -> this.inputTransactions.indexOf(t) == -1).forEach(t -> {
                         double inpt = t.getPigcoins();
                         setTotal_input(inpt);
                 });
 
-                bChain.getBlockChain().stream().filter(t -> t.getpKey_sender().equals(this.address)).forEach(t -> {
+                bChain.getBlockChain().stream().filter(t -> t.getpKey_sender().equals(this.address)).filter(t -> this.outputTransactions.indexOf(t) == -1).forEach(t -> {
                         double oupt = t.getPigcoins();
                         setTotal_output(oupt);
                 });
 
                 setBalance();
-        
         }
 
         @Override
